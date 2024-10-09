92afa2ef0b893d28cf1eede25ace7cc4466f7352
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2024-10-08 23:02:59 -0500

    eliminacion de las carpetas login

--- loginSpring/src/main/java/com/security/springbootsecurityjwt/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.security.springbootsecurityjwt.exceptions;
-
-public class NotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 1;
-
-    public NotFoundException(String message){
-        super(message);
-    }
-}
--- loginSpring/src/main/java/com/security/springbootsecurityjwt/security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.security.springbootsecurityjwt.security;
-
-public class SecurityConstants {
-
-    public static final long JWT_EXPIRATION = 900000; //15 minutos
-}
