a0b369856d7268c3e123181c063ea5c8d6b64034
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-05-08 18:20:48 -0500

    cambios

--- demo/escuela/src/environments/environment.prod.ts
+++ demo/escuela/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
-  production: true,
-  apiUrl: 'mi ruta url '
+  production: false,
+  apiUrl: 'http://localhost:9098/',
+
+  geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent',
+  youtubeApiUrl: 'https://www.googleapis.com/youtube/v3/search',
+  geminiApiKey: 'AIzaSyB9HNN9nYfHK07TlZiCjMG-qVXZ2u70Rxc',
+  youtubeApiKey: 'AIzaSyB9HNN9nYfHK07TlZiCjMG-qVXZ2u70Rxc',
 };
