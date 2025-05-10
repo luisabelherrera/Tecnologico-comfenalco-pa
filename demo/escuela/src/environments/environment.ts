67e1f57a14029a222bf3331a21d506c14981ab7e
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-05-09 22:55:42 -0500

    as

--- demo/escuela/src/environments/environment.ts
+++ demo/escuela/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'backend-production-630e.up.railway.app/',
-
+  production: true,
+  apiUrl: 'https://backend-production-630e.up.railway.app/',  // URL corregida
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent',
   youtubeApiUrl: 'https://www.googleapis.com/youtube/v3/search',
   geminiApiKey: 'AIzaSyB9HNN9nYfHK07TlZiCjMG-qVXZ2u70Rxc',
