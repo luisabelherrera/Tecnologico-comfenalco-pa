86a47d424d937136e0ab59633a259f68df5b7408
abel herrera <140933174+luisabelherrera@users.noreply.github.com>
2025-05-10 00:52:21 -0500

    asd

--- demo/escuela/src/environments/environment.ts
+++ demo/escuela/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eduportal-gamma.vercel.app/',  // URL corregida
+  apiUrl: 'https://proud-celebration-production-d268.up.railway.app/',  // URL corregida
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent',
   youtubeApiUrl: 'https://www.googleapis.com/youtube/v3/search',
   geminiApiKey: 'AIzaSyB9HNN9nYfHK07TlZiCjMG-qVXZ2u70Rxc',
